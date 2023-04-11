9a0efc5f77470e15b905ed7a9f2eb7863c06cffc
Chris Palant <128868182+BeepBoopImChris@users.noreply.github.com>
2023-04-10 21:59:17 -0600

    Update csv-to-pdf.js
    Added validateFile function and removed some comments

--- routes/CSVRoutes/csv-to-pdf.js
+++ routes/CSVRoutes/csv-to-pdf.js
@@ -1,28 +1,28 @@
-// Require Express and create a router instance
+// Import required modules
 const express = require("express");
 const router = express.Router();
-
-// Import middleware and functions for converting CSV to PDF
 const csvUpload = require("../../middleware/csv-upload");
 const { convertCsvToPdf } = require("../../transformers/CSVTransformers/csv-to-pdf/converter");
 const { FileNotFoundError, InvalidFileTypeError } = require("../../transformers/CSVTransformers/csv-to-pdf/customErrors");
 const path = require("path");
 
+// Define a function to validate the uploaded file
+const validateFile = (file) => {
+  if (!file) {
+    throw new FileNotFoundError("No file uploaded.");
+  }
+
+  const fileExtension = path.extname(file.originalname).toLowerCase();
+  if (fileExtension !== ".csv") {
+    throw new InvalidFileTypeError("Invalid file type. Only CSV files are allowed.");
+  }
+};
+
 // Define a route handler for POST requests to /csv-to-pdf
 router.post("/", csvUpload.single("csvfile"), async (req, res, next) => {
   try {
-    // Check if a file was uploaded
-    if (!req.file) {
-      throw new FileNotFoundError("No file uploaded.");
-    }
+    validateFile(req.file);
 
-    // Check if the uploaded file is a CSV
-    const fileExtension = path.extname(req.file.originalname).toLowerCase();
-    if (fileExtension !== ".csv") {
-      throw new InvalidFileTypeError("Invalid file type. Only CSV files are allowed.");
-    }
-
-    // Convert the CSV to PDF and send the download link in the response
     const outputFile = await convertCsvToPdf(req.file.buffer);
     const filename = req.file.originalname.slice(0, -4);
     const downloadLink = `http://${req.headers.host}/output/${path.basename(outputFile)}`;
@@ -33,18 +33,15 @@ router.post("/", csvUpload.single("csvfile"), async (req, res, next) => {
       downloadLink,
     });
   } catch (error) {
-    // Pass any errors to the error handler middleware
     next(error);
   }
 });
 
 // Define an error handler middleware for this router
 router.use((err, req, res, next) => {
-  // If the error is one of our custom errors, send the error message and status code in the response
   if (err instanceof FileNotFoundError || err instanceof InvalidFileTypeError) {
     res.status(err.statusCode).send(err.message);
   } else {
-    // Otherwise, pass the error to the next middleware in the stack
     next(err);
   }
 });
