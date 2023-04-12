f650bffe363cf10023a7f6b8a5848ad4e56ebe10
Chris Palant <128868182+BeepBoopImChris@users.noreply.github.com>
2023-04-11 21:44:06 -0600

    Updated- XLSX to PDF converter
    Output PDF formats better to account for table headers

--- transformers/XLSXTransformers/xlsx-to-pdf/converter.js
+++ transformers/XLSXTransformers/xlsx-to-pdf/converter.js
@@ -9,19 +9,7 @@ if (!fs.existsSync(outputPath)) {
   fs.mkdirSync(outputPath, { recursive: true });
 }
 
-const convertXlsxToPdf = async (buffer, fontColor = '0,0,0') => {
-  const workbook = new ExcelJS.Workbook();
-  await workbook.xlsx.load(buffer);
-
-  const pdfDoc = await PDFDocument.create();
-  const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
-  const fontBold = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
-  const pageWidth = 600;
-  const pageHeight = 800;
-
-  const worksheet = workbook.getWorksheet(1);
-  let pageNumber = 1;
-
+const calculateColumnWidthsAndRowHeights = (worksheet, font) => {
   let columnWidths = [];
   let rowHeights = [];
 
@@ -36,57 +24,83 @@ const convertXlsxToPdf = async (buffer, fontColor = '0,0,0') => {
     });
   });
 
+  return { columnWidths, rowHeights };
+};
+
+const drawTableCell = (page, posX, posY, cellWidth, cellHeight, rowIndex, isEvenRow) => {
+  const isHeader = rowIndex === 1;
+  const borderRadius = 5;
+
+  if (isHeader) {
+    page.drawRectangle({
+      x: posX,
+      y: posY,
+      width: cellWidth,
+      height: cellHeight,
+      color: rgb(0.2, 0.2, 0.2),
+      borderRadius,
+    });
+  } else if (isEvenRow) {
+    page.drawRectangle({
+      x: posX,
+      y: posY,
+      width: cellWidth,
+      height: cellHeight,
+      color: rgb(0.95, 0.95, 0.95),
+      borderRadius,
+    });
+  }
+};
+
+const drawTableCellText = (page, cellValue, posX, posY, cellHeight, rowIndex, font, fontBold, fontColor) => {
+  const fontSize = rowIndex === 1 ? 12 : 10;
+  const textColor = rowIndex === 1 ? rgb(...fontColor.split(',').map(Number)) : rgb(0, 0, 0);
+
+  page.drawText(cellValue, {
+    x: posX + 8, // Add padding to the left of the text
+    y: posY + (cellHeight - fontSize) / 2, // Center text vertically in the cell
+    font: rowIndex === 1 ? fontBold : font,
+    size: fontSize,
+    color: textColor,
+  });
+};
+
+const convertXlsxToPdf = async (buffer, fontColor = '0,0,0') => {
+  const workbook = new ExcelJS.Workbook();
+  await workbook.xlsx.load(buffer);
+
+  const pdfDoc = await PDFDocument.create();
+  const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
+  const fontBold = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
+
+  const worksheet = workbook.getWorksheet(1);
+  let pageNumber = 1;
+
+  const { columnWidths, rowHeights } = calculateColumnWidthsAndRowHeights(worksheet, font);
+
+  const totalWidth = columnWidths.reduce((acc, cur) => acc + cur, 0);
+  const pageWidth = Math.max(totalWidth + 40, 600);
+  const pageHeight = 800;
+
   const rowCount = worksheet.rowCount;
   const marginY = 50;
   let posY = pageHeight - marginY;
-
   const marginLeft = 20;
-
-  let tableWidth = columnWidths.reduce((acc, cur) => acc + cur, 0);
 
   worksheet.eachRow({ includeEmpty: true }, (row, rowIndex) => {
     const page = pdfDoc.getPages()[pageNumber - 1] || pdfDoc.addPage([pageWidth, pageHeight]);
     let posX = marginLeft;
 
     posY -= rowHeights[rowIndex - 1];
+    const isEvenRow = rowIndex % 2 === 0;
 
     row.eachCell({ includeEmpty: true }, (cell, colNumber) => {
       const cellValue = cell.value ? cell.value.toString() : '';
       const cellWidth = columnWidths[colNumber - 1];
       const cellHeight = rowHeights[rowIndex - 1];
-      const fontSize = rowIndex === 1 ? 12 : 10;
-      const textColor = rowIndex === 1 ? rgb(...fontColor.split(',').map(Number)) : rgb(0, 0, 0);
 
-      const isHeader = rowIndex === 1;
-      const isEvenRow = rowIndex % 2 === 0;
-
-      if (isHeader) {
-        page.drawRectangle({
-          x: posX,
-          y: posY,
-          width: cellWidth,
-          height: cellHeight,
-          color: rgb(0.2, 0.2, 0.2),
-          borderRadius: 5,
-        });
-      } else if (isEvenRow) {
-        page.drawRectangle({
-          x: posX,
-          y: posY,
-          width: cellWidth,
-          height: cellHeight,
-          color: rgb(0.95, 0.95, 0.95),
-          borderRadius: 5,
-        });
-      }
-
-      page.drawText(cellValue, {
-        x: posX + 8, // Add padding to the left of the text
-        y: posY + (cellHeight - fontSize) / 2, // Center text vertically in the cell
-        font: rowIndex === 1 ? fontBold : font,
-        size: fontSize,
-        color: textColor,
-      });
+      drawTableCell(page, posX, posY, cellWidth, cellHeight, rowIndex, isEvenRow);
+      drawTableCellText(page, cellValue, posX, posY, cellHeight, rowIndex, font, fontBold, fontColor);
 
       posX += cellWidth;
     });
@@ -106,3 +120,4 @@ const convertXlsxToPdf = async (buffer, fontColor = '0,0,0') => {
 module.exports = {
   convertXlsxToPdf,
 };
+
